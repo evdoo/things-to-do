a56c8515a122f85d9f38d224abdd3407cfdcd228
Olga Evdokimova <o.evdokima@gmail.com>
2023-09-01 03:53:30 +0300

    [fix] Добавлена LiveData

--- app/src/main/java/evdoo/thingstodo/domain/GetThingsListUseCase.kt
+++ app/src/main/java/evdoo/thingstodo/domain/GetThingsListUseCase.kt
@@ -1,6 +1,8 @@
 package evdoo.thingstodo.domain
 
+import androidx.lifecycle.LiveData
+
 class GetThingsListUseCase(private val thingsRepository: ThingsRepository) {
 
-    fun getThingsList() : List<ThingItem> = thingsRepository.getThingsList()
+    fun getThingsList() : LiveData<List<ThingItem>> = thingsRepository.getThingsList()
 }
